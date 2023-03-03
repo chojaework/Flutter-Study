926a29e091ff077fccba274390afefe67074db7c
조재원 <chojaework@gmail.com>
2023-03-03 19:15:52 +0900

    Revert "6.8 Webtoon Card"
    This reverts commit 41de6126b60d0156b0da51f9555f6e944d5302c0.

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -28,34 +28,9 @@ class HomeScreen extends StatelessWidget {
           //builder는 UI를 그려주는 함수
           //snapshot은 Future의 상태 -> 로딩중인지, 데이터가 있는지, 에러가 났는지 알 수 있다
           if (snapshot.hasData) {
-            return ListView.separated(
-              //그냥 ListView를 사용하는 것보다 좀 더 최적화되었다
-              scrollDirection: Axis.horizontal,
-              //스크롤 방향: 수평
-              itemCount: snapshot.data!.length,
-              //총 웹툰의 개수
-              itemBuilder: (context, index) {
-                print(index);
-                //사용자가 보고 있는 아이템만 build가능
-                //사용자가 보고 있지 않으면 ListView.builder는 해당 아이템을 삭제
-                //itemBuilder는 인덱스가 0, 1, 2...인 아이템이 build 됐다고 알려준다
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
-              ),
-              //ListView.separated는 separatorBuilder 위젯을 필수로 한다
-              //이 위젯은 리스트 아이템 사이에 렌더된다(?)
-              //실행하면 width를 20으로 갖고 텍스트가 배치된다
-            );
-            //Text 대신에 ListView 사용
-            //많은 양의 데이터를 연속적으로 보여주고 싶을 때 사용
+            return const Text("There is data!");
           }
-          return const Center(
-            child: CircularProgressIndicator(),
-            //이전에는 Loading...을 print 했다면 이제는 로딩 아이콘이 돌아간다
-          );
+          return const Text('Loading....');
         },
         future: webtoons,
         //future에는 기다려야하는 값인 webtoons를 할당
