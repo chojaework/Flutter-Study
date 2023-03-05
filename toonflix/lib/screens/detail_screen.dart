fc94a4be70bfc2e6b30a1072e030e24f752522b4
조재원 <chojaework@gmail.com>
2023-03-05 18:47:40 +0900

    6.15 Episodes

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -56,79 +56,130 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            //사진을 가운데로 가져온다
+      body: SingleChildScrollView(
+        //scroll 가능한 창을 만들기 위해 사용
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          //포스터 위와 맨 아래 버튼 밑 간격을 50으로 설정
+          //아래의 모든 child한테 같은 간격을 적용하고 싶으므로 Column을 Wrap with Padding 한다
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  //clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    //그냥 이렇게만 설정하면 clipBehavior 때문에 적용이 안되므로 위에 clipBehavior 설정해줘야 한다
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        //번지는 그라데이션 설정
-                        offset: const Offset(10, 10),
-                        //그림자 방향 설정
-                        color: Colors.black.withOpacity(0.5),
-                        //불투명함 설정
-                      )
-                    ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //사진을 가운데로 가져온다
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      //clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        //그냥 이렇게만 설정하면 clipBehavior 때문에 적용이 안되므로 위에 clipBehavior 설정해줘야 한다
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            //번지는 그라데이션 설정
+                            offset: const Offset(10, 10),
+                            //그림자 방향 설정
+                            color: Colors.black.withOpacity(0.5),
+                            //불투명함 설정
+                          )
+                        ],
+                      ),
+                      child: Image.network(widget.thumb),
+                    ),
                   ),
-                  child: Image.network(widget.thumb),
-                ),
+                ],
               ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    );
+                    //"어린 시절 비행기 추락 사고의 유일한 생존자 유이진. 살아남기 위해 용병으로 살아가던 유이진은 10년 후, 가족이 있는 고향으로 돌아왔다." 같이 웹툰 episode 글이 출력된다
+                  }
+                  return const Text("...");
+                  //로딩될 때는 ...이 보인다
+                },
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        for (var episode in snapshot.data!)
+                          Container(
+                            margin: const EdgeInsets.only(
+                              bottom: 10,
+                              //버튼 사이의 간격
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.green.shade400,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 20,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    episode.title,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Icons.chevron_right_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                      ],
+                    );
+                    //만약 보여줘야 할 에피소드의 개수를 모른다면 ListView를 사용해도 좋다
+                    //하지만 에피소드의 개수가 10개로 정해져 있으므로 여기서는 Column 사용한다
+                  }
+                  return Container();
+                },
+              )
             ],
           ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        snapshot.data!.about,
-                        style: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-                //"어린 시절 비행기 추락 사고의 유일한 생존자 유이진. 살아남기 위해 용병으로 살아가던 유이진은 10년 후, 가족이 있는 고향으로 돌아왔다." 같이 웹툰 episode 글이 출력된다
-              }
-              return const Text("...");
-              //로딩될 때는 ...이 보인다
-            },
-          )
-        ],
+        ),
       ),
     );
   }
